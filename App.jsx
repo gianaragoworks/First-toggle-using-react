be6b1a9e1653d07bff0f405aa78c601b206517df
Gian <gian94794@gmail.com>
2026-08-30 12:45:41 +0800

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,22 +1,15 @@
 import { useState } from 'react';
 import './App.css'
 
-function Greetings({ name, age }) {
-  return (
-    <h1>Hello my name is {name} and I am {age} years old</h1>
-  );
-}
-
 function App() {
-  const [showGreetingss, setshowGreetingss] = useState(false);
-  function toggleGreeting (){
-    setshowGreetingss(!showGreetingss);
+  const [Name, setName] = useState("")
+
+  function handleChange(event) {
+    const value = event.target.value;
   }
   return (
     <div>
-      <button onClick={toggleGreeting}>Toggle Greeting</button>
-      {showGreetingss && (<Greetings name = {"Gian"} age = {18} />
-      )}
+      <input type="text" placeholder="Name....." onChange={handleChange} />
     </div>
     
   );
