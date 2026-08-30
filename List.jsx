d5b5e4a79a8de1ddfd4c0812e9e6d3efacdfba25
Gian <gian94794@gmail.com>
2026-08-30 13:49:19 +0800

    Add TodoList component with sample todos

---
+++ List.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import './App.css'
+
+function TodoList(){
+  const todos = [
+    {id: 1, text: "Make a cup of coffee"},
+    {id: 2, text: "Get some piece of bread"},
+    {id: 3, text: "Go to school"}
+    ];
+  
+  return (
+    <div>
+    <h2>My To do List</h2>
+      <ul>
+        {todos.map((todo) => (
+      <li key={todo.id}>{todo.text}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function App(){
+  return(
+    <div>
+    <TodoList />
+    </div>
+  );
+}
+
+
+export default App
